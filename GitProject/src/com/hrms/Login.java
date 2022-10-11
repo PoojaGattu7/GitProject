aecc616893d5da217ddad44e6eae992c3a501570
Pooja Gattu <poojagattu777@gmail.com>
2022-10-11 20:56:40 +0530

    Created Pojo Class

---
+++ GitProject/src/com/hrms/Login.java
@@ -0,0 +1,19 @@
+package com.hrms;
+
+public class Login {
+    public int rollNO;
+    public String empName;
+	public int getRollNO() {
+		return rollNO;
+	}
+	public void setRollNO(int rollNO) {
+		this.rollNO = rollNO;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+    
+}
